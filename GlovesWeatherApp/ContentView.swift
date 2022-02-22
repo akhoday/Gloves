80d2fbe7ba3e99a1af312186fbf5871db335ec04
Alveena Khoday <s015846@students.lmsd.org>
2022-02-22 08:27:28 -0500

    Initial Commit

---
+++ GlovesWeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GlovesWeatherApp
+//
+//  Created by Alveena Khoday (student LM) on 2/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GlovesWeatherApp/GlovesWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlovesWeatherAppApp.swift
+//  GlovesWeatherApp
+//
+//  Created by Alveena Khoday (student LM) on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct GlovesWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
