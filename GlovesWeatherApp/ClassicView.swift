ee2952e7c27590aaeb822c6b0a826d5412c4ec07
Maria Dymek <mdymek803@gmail.com>
2022-03-30 09:07:56 -0400

    new views

---
+++ GlovesWeatherApp/ClassicView.swift
@@ -0,0 +1,20 @@
+//
+//  ClassicView.swift
+//  GlovesWeatherApp
+//
+//  Created by Maria Dymek (student LM) on 3/30/22.
+//
+
+import SwiftUI
+
+struct ClassicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ClassicView_Previews: PreviewProvider {
+    static var previews: some View {
+        ClassicView()
+    }
+}
