8808f298c9177a5ee0ad39e0509068c143cf52ec
Alveena Khoday <s015846@students.lmsd.org>
2022-04-05 13:29:45 -0400

    clothing dictionaries

--- GlovesWeatherApp/ClothingTime.swift
+++ GlovesWeatherApp/ClothingTime.swift
@@ -27,169 +27,95 @@ var extra = ""
 var temp = (data.temp)!
 var snow = (data.snow)!
 
-var coldColdComfy = [shirts: "", pants: "", coats: "", socks: "", shoes: "boots (uggs), sneakers(forces)", gloves: "recommended", scarves: "recommended", hats: "beanie, ear muffs, ski mask, head warmer headbands", sunglasses: "optional", extra: ""]
+var coldColdComfy = [shirts: "long sleeve thick shirt or a short sleeve that is a good to layer w/ sweatshirt/jacket(zip up)", pants: "sweatpants, thick leggings", coats: "heavy winter coat (canada goose, puffer jacket)", socks: " 2 pairs of socks or thick socks", shoes: "boots (uggs), sneakers(forces)", gloves: "recommended", scarves: "recommended", hats: "beanie, ear muffs, ski mask, head warmer headbands", sunglasses: "optional", extra: ""]
 
-var coldColdFund = [shirts: "", pants: "", coats: "", socks: "", shoes: "boots (uggs), sneakers(forces)", gloves: "recommended", scarves: "recommended", hats: "beanie, ear muffs, ski mask, head warmer headbands", sunglasses: "optional", extra: ""]
+var coldColdFund = [shirts: "long sleeve thick shirt, sweater or a short sleeve that is a good to layer w/ a jacket", pants: "thick leggings, thick jeans, thick corduroy pants", coats: "heavy winter coat (canada goose, puffer jacket)", socks: " 2 pairs of socks or thick socks", shoes: "boots (uggs, docs), sneakers(forces)", gloves: "recommended", scarves: "recommended", hats: "beanie, ear muffs, ski mask, head warmer headbands", sunglasses: "optional", extra: ""]
 
-var coldComfy = [shirts: "", pants: "", coats: "", socks: "", shoes: "boots(uggs), sneakers(forces, converse, vans)", gloves: "", scarves: "", hats: "beanie, ear muffs, ski mask, head warmer headbands", sunglasses: "optional", extra: ""]
+var coldComfy = [shirts: "long sleeve thick shirt or a short sleeve that is a good to layer w/ sweatshirt/jacket(zip up)", pants: "sweatpants, leggings", coats: "heavy winter coat (canada goose, puffer jacket)", socks: "a good pair of socks", shoes: "boots(uggs), sneakers(forces, converse, vans)", gloves: "", scarves: "recommended", hats: "beanie, ear muffs, ski mask, head warmer headbands", sunglasses: "optional", extra: ""]
 
-var coldFund = [shirts: "", pants: "", coats: "", socks: "", shoes: "boots(uggs), sneakers(forces, converse, vans)", gloves: "recommended", scarves: "recommended", hats: "beanie, ear muffs, ski mask, head warmer headbands", sunglasses: "optional", extra: ""]
-
-
-var coldishComfy = [shirts: "", pants: "", coats: "", socks: "", shoes: " sneakers(forces, converse, vans, nike, adidas)", gloves: "optional", scarves: "optional", hats: "optional winter hat", sunglasses: "optional", extra: ""]
-
-var coldishFund = [shirts: "", pants: "", coats: "", socks: "", shoes: " sneakers(forces, converse, vans, nike, adidas)", gloves: "optional", scarves: "optional", hats: "optional winter hat", sunglasses: "optional", extra: ""]
+var coldFund = [shirts: "long sleeve thick shirt, sweater or a short sleeve that is a good to layer w/ a jacket", pants: "leggings, jeans, corduroy", coats: "heavy winter coat (canada goose, puffer jacket)", socks: "a good pair of socks", shoes: "boots(uggs, docs), sneakers(forces, converse, vans)", gloves: "recommended", scarves: "recommended", hats: "beanie, ear muffs, ski mask, head warmer headbands", sunglasses: "optional", extra: ""]
 
 
-var warmishComfy = [shirts: "", pants: "", coats: "", socks: "", shoes: "sneakers(forces, conserve, vans, nike, adidas)", gloves: "", scarves: "", hats: "", sunglasses: "optional", extra: ""]
+var coldishComfy = [shirts: "long sleeve shirt or a short sleeve that is a good to layer w/ sweatshirt/jacket(zip up)", pants: "sweatpants, leggings", coats: "basic winter coat (north face)", socks: "", shoes: "sneakers(forces, converse, vans, nike, adidas)", gloves: "optional", scarves: "optional", hats: "optional winter hat", sunglasses: "optional", extra: ""]
 
-var warmishFund = [shirts: "", pants: "", coats: "", socks: "", shoes: "sneakers(forces, conserve, vans, nike, adidas)", gloves: "", scarves: "", hats: "baseball cap", sunglasses: "optional", extra: ""]
-
-
-var warmerComfy = [shirts: "", pants: "", coats: "", socks: "", shoes: " sneakers(forces, conserve, vans, nike, adidas)", gloves: "", scarves: "", hats: "baseball cap", sunglasses: "optional", extra: ""]
-
-var warmerFund = [shirts: "", pants: "", coats: "", socks: "", shoes: " sneakers(forces, conserve, vans, nike, adidas)", gloves: "", scarves: "", hats: "baseball cap", sunglasses: "optional", extra: ""]
+var coldishFund = [shirts: "long sleeve shirt, sweater or a short sleeve that is a good to layer w/ a jacket", pants: "leggings, jeans, corduroy", coats: "basic winter coat (north face)", socks: "normal socks", shoes: "sneakers(forces, converse, vans, nike, adidas), docs", gloves: "optional", scarves: "optional", hats: "optional winter hat", sunglasses: "optional", extra: ""]
 
 
-var hotComfy = [shirts: "", pants: "", coats: "", socks: "", shoes: "sneakers (converse, forces, vans), birkenstocks", gloves: "", scarves: "", hats: "baseball cap", sunglasses: "optional", extra: ""]
+var warmishComfy = [shirts: "short sleeve w/ jacket, or long sleeve shirt", pants: " sweatpants, leggings, potentially shorts", coats: "lighter/fleece jacket", socks: "normal socks", shoes: "sneakers(forces, conserve, vans, nike, adidas)", gloves: "not recommended", scarves: "not recommended", hats: "", sunglasses: "optional", extra: ""]
 
-var hotFund = [shirts: "", pants: "", coats: "", socks: "", shoes: "sneakers (converse, forces, vans), birkenstocks", gloves: "", scarves: "", hats: "baseball cap", sunglasses: "optional", extra: ""]
-
-var hawtComfy = [shirts: "", pants: "", coats: "", socks: "", shoes: "flip flops, birkenstocks, sandals, sneakers", gloves: "", scarves: "", hats: "baseball cap", sunglasses: "optional", extra: ""]
-
-var hawtFund = [shirts: "", pants: "", coats: "", socks: "", shoes: "flip flops, birkenstocks, sandals, sneakers", gloves: "", scarves: "", hats: "baseball cap", sunglasses: "optional", extra: ""]
+var warmishFund = [shirts: "Long sleeve shirt, sweater, or short sleeve shirt layered over long sleeve", pants: "jeans, leggings, optional shorts", coats: "lighter/fleece jacket", socks: "normal socks", shoes: "sneakers(forces, conserve, vans, nike, adidas)", gloves: "not recommended", scarves: "not recommended", hats: "baseball cap", sunglasses: "optional", extra: ""]
 
 
+var warmerComfy = [shirts: "casual t-shirt, long sleeve ", pants: "athletic shorts, sweat shorts", coats: "optional jacket", socks: "normal socks", shoes: "sneakers(forces, conserve, vans, nike, adidas)", gloves: "not recommended", scarves: "not recommended", hats: "baseball cap", sunglasses: "optional", extra: ""]
 
+var warmerFund = [shirts: "oversized t-shirt, casual long sleeve", pants: "jean shorts", coats: "optional jacket", socks: "normal socks", shoes: "sneakers(forces, conserve, vans, nike, adidas)", gloves: "not recommended", scarves: "not recommended", hats: "baseball cap", sunglasses: "optional", extra: ""]
 
+var hotComfy = [shirts: "casual t-shirt, tank tops", pants: "athletic shorts, sweat shorts", coats: "optional jacket", socks: "athletic socks, cotton socks", shoes: "sneakers (converse, forces, vans), birkenstocks", gloves: "not recommended", scarves: "not recommended", hats: "baseball cap", sunglasses: "optional", extra: ""]
 
+var hotFund = [shirts: "oversized t-shirt, tank tops", pants: " jean shorts, skirt", coats: "optional jacket", socks: "athletic socks, cotton socks", shoes: "sneakers (converse, forces, vans), birkenstocks", gloves: "not recommended", scarves: "not recommended", hats: "baseball cap", sunglasses: "optional", extra: ""]
 
+var hawtComfy = [shirts: "casual t-shirt, tank tops", pants: "athletic shorts, sweat shorts", coats: "optional jacket", socks: "no socks or socks w/ sneakers", shoes: "flip flops, birkenstocks, sandals, sneakers", gloves: "not recommended", scarves: "not recommended", hats: "baseball cap", sunglasses: "optional", extra: ""]
 
+var hawtFund = [shirts: "oversized t-shirt, tank tops", pants: " jean shorts, skirt", coats: "optional jacket", socks: "no socks or socks w/ sneakers", shoes: "flip flops, birkenstocks, sandals, sneakers", gloves: "not recommended", scarves: "not recommended", hats: "baseball cap", sunglasses: "optional", extra: ""]
 
-
-
-
-
-
-/*
-func comfy()-> String{
-    
+func rainOrSnow()-> String{
     
     if (data.precip)! > 2.4{
         isRaining = true
     }
   
-    //shoes for every temperature
-    if temp < 21 || (temp > 22 && temp < 33){
-        if isRaining == true {
-            shoes = "Rain boots, Snow boots, Waterproof Shoes"
-        }
+    if isRaining == true {
+        coldColdComfy[shoes] = "rain boots, snow boots, waterproof shoes"
+        coldColdFund[shoes] = "rain boots, snow boots, waterproof shoes"
+        coldComfy[shoes] = "rain boots, snow boots, waterproof shoes"
+        coldFund[shoes] = "rain boots, snow boots, waterproof shoes"
+        coldishComfy[shoes] = "rain boots"
+        coldishFund[shoes] = "rain boots"
+        warmishFund[shoes] = "rain boots, old sneakers"
+        warmishComfy[shoes] = "rain boots, old sneakers"
+        warmerFund[shoes] = "old sneakers"
+        warmerComfy[shoes] = "old sneakers"
+        hotFund[shoes] = "old sneakers"
+        hotComfy[shoes] = "old sneakers"
+        hawtFund[shoes] = "old sneakers"
+        hawtComfy[shoes] = "old sneakers"
         
-        if snow > 0 {
-            shoes = "Snow boots, Shoes w/ good soles"
-        }
-        else {
-            shoes = "Boots(maybe Uggs), or Sneakers(like forces), Vans"
-        }
+        coldColdComfy[extra] = "Bring an umbrella!"
+        coldColdFund[extra] = "Bring an umbrella!"
+        coldComfy[extra] = "Bring an umbrella!"
+        coldFund[extra] = "Bring an umbrella!"
+        coldishComfy[extra] = "Bring an umbrella!"
+        coldishFund[extra] = "Bring an umbrella!"
+        warmishFund[extra] = "Bring an umbrella!"
+        warmishComfy[extra] = "Bring an umbrella!"
+        warmerFund[extra] = "Bring an umbrella!"
+        warmerComfy[extra] = "Bring an umbrella!"
+        hotFund[extra] = "Bring an umbrella!"
+        hotComfy[extra] = "Bring an umbrella!"
+        hawtFund[extra] = "Bring an umbrella!"
+        hawtComfy[extra] = "Bring an umbrella!"
+    }
+    
+    if snow > 0 {
+        coldColdComfy[shoes] = "snow boots, good sole shoes"
+        coldColdFund[shoes] = "snow boots, good sole shoes"
+        coldComfy[shoes] = "snow boots, good sole shoes"
+        coldFund[shoes] = "snow boots, good sole shoes"
+        coldishComfy[shoes] = "no need for snow boots but good sole shoes"
+        coldishFund[shoes] = "no need for snow boots but good sole shoes"
     }
     
     
-    if temp > 34 && temp < 51 {
-        if isRaining == true {
-            shoes = "Rainboots, Old Sneakers"
-        }
-        
-        if snow > 0 {
-            shoes = "No need for snow boots but good sole shoes"
-        }
-        else {
-            shoes = "Boots(maybe Uggs), or Sneakers(Air Forces, Converse, Vans, Nike, Adidas) "
-        }
-        
-    }
-    
-    if temp > 50 && temp < 61{
-        if isRaining == true {
-            
-        }
-        
-        if snow > 0 {
-            
-        }
-        else {
-            shoes = "Boots(maybe Uggs), or Sneakers(like forces)"
-        }
-        
-    }
-    
-    
-    if (temp > 60 && temp < 71) || (temp > 70 && temp < 86) {
-        if isRaining == true {
-            
-        }
-        
-        if snow > 0 {
-            
-        }
-        else {
-            shoes = "Boots(maybe Uggs), or Sneakers(like forces)"
-        }
-        
-    }
-    
-    if temp > 85 {
-        if isRaining == true {
-            
-        }
-        
-        if snow > 0 {
-            
-        }
-        else {
-            shoes = "Boots(maybe Uggs), or Sneakers(like forces)"
-        }
-        
-    }
-    
-    return shoes */
+    return "ur mom"
+}
 
-/*
- if isRaining == true{
-    shoes = "rain boots, snow boots, or overall waterproof shoes"
- }*/
-// light rain: precipitation rate is < 2.5 mm (0.098 in) per hour
-// Moderate rain — when the precipitation rate is between 2.5 mm - 7.6 mmper hour
-// heavy rain :  > 7.6 mm per hour
 
-//change so its not force unwrapping eventually
 
-/*
- if temp < 21 {
-     
- }
- 
- if temp > 22 && temp < 33{
- }
- 
- if temp > 34 && temp < 51 {
-     
- }
- 
- if temp > 50 && temp < 61{
-     
- }
- 
- if temp > 60 && temp < 71 {
-     
- }
- 
- if temp > 70 && temp < 86 {
-     
- }
- 
- if temp > 85 {
-     
- }
- 
- */
+
+
+
+
+
+
+
 
