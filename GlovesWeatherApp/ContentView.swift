aa23779aec7144380945d86392140c2d7a139480
Barney <s016602@students.lmsd.org>
2022-04-06 10:30:03 -0400

    content view formatting and adding location

--- GlovesWeatherApp/ContentView.swift
+++ GlovesWeatherApp/ContentView.swift
@@ -13,49 +13,60 @@ struct ContentView: View {
     var body: some View {
         
         //will have the background based on the weather behind all of the info
-      
+        
+        var address = fetchData.responses.city_name
+        var stateCode = fetchData.responses.state_code
+        ZStack{
+            Rectangle().frame(width: 400.0, height: 75.0).foregroundColor(Color.background).edgesIgnoringSafeArea(.all)
+            VStack {
                 
+                Button("Change location") {
+                    
+                }.foregroundColor(Color.buttonColor)
+                Text((address ?? "no idea sorry man") + ", " + (stateCode ?? "what is even happening")).foregroundColor(Color.fontColor)
                 
-                //next couple of days and that array
+            }
+        }
+        //next couple of days and that array
+        ZStack{
+            
+            NavigationView{
                 ZStack{
                     
-                    NavigationView{
-                        ZStack{
-                            
-                            List(fetchData.responses.data) {
-                                
-                                data in
-                                if #available(iOS 15.0, *) {
-                                    NavigationLink(
-                                        destination: DailyInfoView(weather: data.weather, response: fetchData.responses, data: data),
-                                        label: {
-                                            
-                                            WeatherInfoView(weather: data.weather, response: fetchData.responses, data: data)
-                                            
-                                            
-                                        }).listRowBackground(Color.background).listRowSeparatorTint(Color.fontColor)
-                                        .navigationTitle(Text("16 Day Forecast")).accentColor(Color.fontColor)
-                                } else {
-                                    NavigationLink(
-                                        destination: DailyInfoView(weather: data.weather, response: fetchData.responses, data: data),
-                                        label: {
-                                            
-                                            WeatherInfoView(weather: data.weather, response: fetchData.responses, data: data)
-                                            
-                                            
-                                        }).listRowBackground(Color.background)
-                                }
-                            }
-                            
+                    List(fetchData.responses.data) {
+                        
+                        data in
+                        if #available(iOS 15.0, *) {
+                            NavigationLink(
+                                destination: DailyInfoView(weather: data.weather, response: fetchData.responses, data: data),
+                                label: {
+                                    
+                                    WeatherInfoView(weather: data.weather, response: fetchData.responses, data: data)
+                                    
+                                    
+                                }).listRowBackground(Color.background).listRowSeparatorTint(Color.fontColor)
+                                .navigationTitle(Text("16 Day Forecast")).accentColor(Color.fontColor)
+                        } else {
+                            NavigationLink(
+                                destination: DailyInfoView(weather: data.weather, response: fetchData.responses, data: data),
+                                label: {
+                                    
+                                    WeatherInfoView(weather: data.weather, response: fetchData.responses, data: data)
+                                    
+                                    
+                                }).listRowBackground(Color.background)
                         }
                     }
+                    
                 }
             }
-            
         }
-        
-        
+    }
     
+}
+
+
+
 
 
 struct ContentView_Previews: PreviewProvider {
